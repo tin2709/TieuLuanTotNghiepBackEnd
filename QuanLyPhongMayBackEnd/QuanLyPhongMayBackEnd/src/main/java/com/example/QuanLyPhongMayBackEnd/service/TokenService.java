8980c3635aba75fa346b43a454f66cfb187debb9
tin2709 <trungtinpham336@gmail.com>
2025-02-14 10:55:53 +0700

    them phuong thuc tao token, xac thuc token va api login va checklogin

--- QuanLyPhongMayBackEnd/QuanLyPhongMayBackEnd/src/main/java/com/example/QuanLyPhongMayBackEnd/service/TokenService.java
+++ QuanLyPhongMayBackEnd/QuanLyPhongMayBackEnd/src/main/java/com/example/QuanLyPhongMayBackEnd/service/TokenService.java
@@ -1,5 +1,7 @@
 package com.example.QuanLyPhongMayBackEnd.service;
 
+import com.example.QuanLyPhongMayBackEnd.entity.Token;
+import com.example.QuanLyPhongMayBackEnd.entity.TaiKhoan;
 import com.example.QuanLyPhongMayBackEnd.repository.TokenRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +9,33 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 @Service
 public class TokenService {
 
     @Autowired
     private TokenRepository tokenRepository;
 
-    // Phương thức này sẽ được gọi mỗi giờ để xóa các token đã hết hạn
+    @Autowired
+    private TaiKhoanService taiKhoanService;
+
+    // Lưu token cho người dùng
+    @Transactional
+    public Token saveToken(String token, TaiKhoan taiKhoan) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expiresAt = now.plusHours(1); // Token sẽ hết hạn sau 1 giờ
+
+        Token newToken = new Token(token, now, expiresAt, taiKhoan);
+        return tokenRepository.save(newToken);
+    }
+
+
+
+
+    // Xóa các token đã hết hạn
     @Scheduled(fixedRate = 3600000) // 1 giờ = 3600000 milliseconds
     @Transactional
     public void deleteExpiredTokens() {
