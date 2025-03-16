d784092e02b54b21ec1d23a264feba685143b1d2
tin2709 <trungtinpham336@gmail.com>
2025-03-16 16:39:18 +0700

    them server-side-event de hien thi thong bao ngay khi xoa tang

--- QuanLyPhongMayBackEnd/QuanLyPhongMayBackEnd/src/main/java/com/example/QuanLyPhongMayBackEnd/QuanLyPhongMayBackEndApplication.java
+++ QuanLyPhongMayBackEnd/QuanLyPhongMayBackEnd/src/main/java/com/example/QuanLyPhongMayBackEnd/QuanLyPhongMayBackEndApplication.java
@@ -4,6 +4,9 @@ import io.sentry.Sentry;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuanLyPhongMayBackEndApplication implements CommandLineRunner {
@@ -21,5 +24,18 @@ public class QuanLyPhongMayBackEndApplication implements CommandLineRunner {
 			Sentry.captureException(e);  // Gửi lỗi này đến Sentry
 			System.out.println("Exception sent to Sentry");
 		}
+	}
+	@Bean // VERY IMPORTANT: Global CORS configuration
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Apply to all endpoints
+						.allowedOrigins("http://localhost:3000") // Your React app's origin
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+						.allowedHeaders("*") // Allowed headers
+						.allowCredentials(true); // If you need to send cookies
+			}
+		};
 	}
 }
